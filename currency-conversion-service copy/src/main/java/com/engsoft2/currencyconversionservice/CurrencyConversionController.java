566c3b989810f177ac4a703f39f1c68f96abb311
pedroFonseca447 <pedrofonseca447@gmail.com>
2023-05-18 22:06:26 -0300

    dei uma olhada no gepeto mas n testei ainda se ta 100%

--- currency-conversion-service copy/src/main/java/com/engsoft2/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service copy/src/main/java/com/engsoft2/currencyconversionservice/CurrencyConversionController.java
@@ -1,80 +1,92 @@
 package com.engsoft2.currencyconversionservice;
 
 import java.math.BigDecimal;
-
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.PathVariable;
-
 import org.springframework.web.bind.annotation.RestController;
-
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-
-public class CurrencyConversionCollector {
+public class CurrencyCollectorController {
 
     @Autowired
-
     private CurrencyExchangeProxy proxy;
 
+    private List<Information> informationList = new ArrayList<>();
+
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
-
     public CurrencyConversion calculateCurrencyConversion(
-
             @PathVariable String from,
-
             @PathVariable String to,
-
             @PathVariable BigDecimal quantity) {
 
         HashMap<String, String> uriVariables = new HashMap<>();
-
         uriVariables.put("from", from);
-
         uriVariables.put("to", to);
 
         ResponseEntity<CurrencyConversion> responseEntity =
-
                 new RestTemplate().getForEntity(
-
                         "http://localhost:8000/currency-exchange/from/{from}/to/{to}",
-
                         CurrencyConversion.class,
-
                         uriVariables);
 
         CurrencyConversion currencyConversion = responseEntity.getBody();
 
+        // Coletar informações da consulta
+        Information information = new Information();
+        information.setCurrency(currencyConversion.getFrom() + " to " + currencyConversion.getTo());
+        information.setQuantity(quantity);
+        information.setValue(currencyConversion.getConversionMultiple());
+        information.setTimestamp(LocalDateTime.now());
+
+        informationList.add(information);
+
         return new CurrencyConversion(
-
                 currencyConversion.getId(),
-
-                from, to, quantity,
-
+                from,
+                to,
+                quantity,
                 currencyConversion.getConversionMultiple(),
-
                 quantity.multiply(currencyConversion.getConversionMultiple()),
-
                 currencyConversion.getEnvironment() + " " + "rest template"
-
         );
-
     }
 
     @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from, @PathVariable String to,
             @PathVariable BigDecimal quantity) {
         CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
-        return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
+
+        // Coletar informações da consulta
+        Information information = new Information();
+        information.setCurrency(currencyConversion.getFrom() + " to " + currencyConversion.getTo());
+        information.setQuantity(quantity);
+        information.setValue(currencyConversion.getConversionMultiple());
+        information.setTimestamp(LocalDateTime.now());
+
+        informationList.add(information);
+
+        return new CurrencyConversion(
+                currencyConversion.getId(),
+                from,
+                to,
+                quantity,
                 currencyConversion.getConversionMultiple(),
                 quantity.multiply(currencyConversion.getConversionMultiple()),
-                currencyConversion.getEnvironment() + " " + "feign");
+                currencyConversion.getEnvironment() + " " + "feign"
+        );
+    }
+
+    @GetMapping("/information")
+    public List<Information> getAllInformation() {
+        return informationList;
     }
 }
